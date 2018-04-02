423f26d55e45cfaeace442f2b89b24b8465e788f
dnbaker <dnb@cs.jhu.edu>
2018-04-02 02:04:31 -0400

    Some cleanup.

--- vector.h
+++ vector.h
@@ -4,6 +4,8 @@
 #include <stdexcept>
 #include <cstring>
 #include <cstdlib>
+#include <memory>
+#include "kspp/ks.h"
 
 #ifndef LAZY_PUSH_BACK_RESIZING_FACTOR
 #define LAZY_PUSH_BACK_RESIZING_FACTOR 1.25
@@ -16,7 +18,7 @@ enum initialization: bool {
     LAZY_VEC_INIT = true
 };
 
-template<typename T, typename size_type=std::size_t>
+template<typename T, typename size_type=uint32_t>
 class vector {
     size_type n_, m_;
     T *data_;
@@ -25,10 +27,20 @@ class vector {
 public:
     using value_type = T;
     template<typename... FactoryArgs>
-    vector(size_type n=0, bool init=!std::is_pod<T>::value, FactoryArgs &&...args): n_{n}, m_{n}, data_{static_cast<T *>(std::malloc(sizeof(T) * n))} {
+    vector(size_type n=0, bool init=!std::is_pod_v<T>, FactoryArgs &&...args): n_{n}, m_{n}, data_{n ? static_cast<T *>(std::malloc(sizeof(T) * n)): nullptr} {
         if (init)
             for(size_type i(0); i < n_; ++i)
                 new(data_ + i) T(std::forward<FactoryArgs>(args)...);
+    }
+    vector(const vector &other): n_(other.n_), m_(other.n_), data_{static_cast<T *>(std::malloc(sizeof(T) * m_))} {
+        std::copy(std::cbegin(other), std::cend(other), begin());
+    }
+    vector(vector &&other): n_(other.n_), m_(other.m_), data_(other.data_) {
+        other.m_ = other.n_ = 0;
+        other.data_ = nullptr;
+    }
+    vector(std::initializer_list<T> il): n_(il.size()), m_(il.size()), data_{n_ ? static_cast<T *>(std::malloc(sizeof(T) * n_)): nullptr} {
+        std::move(il.begin(), il.end(), std::begin(*this));
     }
     const T *cbegin() const {return data_;}
     const T *cend()   const {return data_ + n_;}
@@ -44,6 +56,20 @@ public:
     const auto &front() const {return data_[0];}
     auto size() const {return n_;}
     auto capacity() const {return m_;}
+    vector &operator=(const vector &o) {
+        if(m_ < o.m_) data_ = (T *)std::realloc(data_, sizeof(T) * o.m_);
+        n_ = o.n_;
+        m_ = o.m_;
+        std::copy(begin(), end(), (T *)o.begin());
+        return *this;
+    }
+    vector &operator=(vector &&o) {
+        n_ = o.n_;
+        m_ = o.m_;
+        data_ = o.data_;
+        std::memset(&o, 0, sizeof(o));
+        return *this;
+    }
     template<typename osize_type>
     bool operator==(const ::lazy::vector<T, osize_type> &other) const {
         if(size() != other.size()) return false;
@@ -52,27 +78,32 @@ public:
                 return false;
         return true;
     }
-    // Note: push_back and emplace_back are the same *except* that push_back changes size multiplicatively.
+    // Note: push_back and bnslace_back are the same *except* that push_back changes size multiplicatively.
     template<typename... Args>
     auto &emplace_back(Args&& ... args) {
         if(n_ + 1 > m_) {
             data_ = static_cast<T *>(std::realloc(data_, sizeof(T) * (n_ + 1)));
+#if !NDEBUG
+            if(m_ + 1 < m_) throw std::runtime_error(
+                ks::sprintf("Type of size %zu is not big enough. (%zu, %zu)\n",
+                            sizeof(m_), m_, m_ + 1).data());
+#else
             ++m_;
+#endif
         }
         new(data_ + n_++) T(std::forward<Args>(args)...);
         return back();
     }
     template<typename... Args>
     auto &push_back(Args&& ... args) {
-        if(n_ + 1 > m_) {
-            m_ = std::max(static_cast<size_type>(m_ * PUSH_BACK_RESIZING_FACTOR), m_ + 1);
-            data_ = static_cast<T *>(std::realloc(data_, sizeof(T) * (m_)));
-        }
+        if(n_ + 1 > m_)
+            reserve(std::max(static_cast<size_type>(m_ * PUSH_BACK_RESIZING_FACTOR),
+                             m_ + 1));
         new(data_ + n_++) T(std::forward<Args>(args)...);
         return back();
     }
     void zero() {std::memset(data_, 0, sizeof(T) * n_);}
-    void reserve(std::size_t newsize) {
+    void reserve(size_t newsize) {
         if(newsize > m_) {
             auto tmp(static_cast<T*>(std::realloc(data_, sizeof(T) * newsize)));
             if(tmp == nullptr) throw std::bad_alloc();
@@ -80,7 +111,7 @@ public:
         }
     }
     template<typename... Args>
-    void resize(std::size_t newsize, bool init=true, Args &&...args) {
+    void resize(size_t newsize, bool init=true, Args &&...args) {
         reserve(newsize);
         if(init) {
             for(;n_ < m_;) new(data_ + n_++) T(std::forward<Args>(args)...);
@@ -91,12 +122,13 @@ public:
             auto tmp(static_cast<T*>(std::realloc(data_, sizeof(T) * n_)));
             if(tmp == nullptr) throw std::bad_alloc();
             data_ = tmp;
+            m_ = n_;
         }
     }
     T &operator[](size_type idx) {return data_[idx];}
     const T &operator[](size_type idx) const {return data_[idx];}
     ~vector(){
-        if constexpr(!std::is_trivially_destructible<T>::value) {
+        if constexpr(!std::is_trivially_destructible_v<T>) {
             for(auto &el: *this) el.~T();
         }
         std::free(data_);
